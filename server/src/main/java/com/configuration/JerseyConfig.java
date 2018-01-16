b40a40d12686099b5fa5131f20e7d0837dde0990
Eduardo <barrenaedu@gmail.com>
2018-01-16 16:06:58 -0300

    Fixing spring boot run from jar method

--- server/src/main/java/com/configuration/JerseyConfig.java
+++ server/src/main/java/com/configuration/JerseyConfig.java
@@ -1,17 +1,18 @@
 package com.configuration;
 
-import javax.ws.rs.ApplicationPath;
-
+import com.rest.MessagesResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.springframework.context.annotation.Configuration;
+
+import javax.ws.rs.ApplicationPath;
 
 @Configuration
 @ApplicationPath("/rest")
 public class JerseyConfig extends ResourceConfig {
 	
 	public JerseyConfig() {
-		packages("com.rest");
+		register(MessagesResource.class);
 		property(ServerProperties.WADL_FEATURE_DISABLE, true);
 	}
 
